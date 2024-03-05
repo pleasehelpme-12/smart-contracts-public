31fa9e8f941901a48dc8e8719f10a3c1cfe8c084
pleasehelpme-12 <106196653+pleasehelpme-12@users.noreply.github.com>
2024-03-05 13:03:15 +0100

    Update Vis.sol

--- contracts/token/Vis.sol
+++ contracts/token/Vis.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/0x540C605a35b943E12c928b1B5f2cb7df2C403409/token/ERC20/ERC20.sol";
+import "@openzeppelin/0x540C605a35b943E12c928b1B5f2cb7df2C403409/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract Vis is ERC20, ERC20Burnable {
     
